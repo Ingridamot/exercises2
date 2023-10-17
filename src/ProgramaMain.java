5dd0ec9ddd5e7ad5d89724d94e4bb1f78eaf0eaf
Ingridamot <148130408+Ingridamot@users.noreply.github.com>
2023-10-17 21:56:39 +0300

    Update ProgramaMain.java
    Padaryti_pakeitimai_Git_puslapyje

--- src/ProgramaMain.java
+++ src/ProgramaMain.java
@@ -1,5 +1,6 @@
 public class ProgramaMain {
     public static void main(String[] args) {
         System.out.println("Hello New world");
+        System.out.println("Pakeistas failas Git puslapyje");
     }
 }
